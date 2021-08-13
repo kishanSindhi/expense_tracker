11c42de0c275959df549dd6e177aa96540b2273e
Kishan Sindhi <ksindhi474@gmail.com>
2021-08-13 11:25:04 +0530

    transaction page added and beautify the amount

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './transaction.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -9,12 +11,116 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter App",
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final List<Transaction> transaction = [
+    Transaction(
+      id: "t1",
+      title: "New Shoes",
+      amount: 39.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: "t2",
+      title: "Groceries",
+      amount: 30.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: "t3",
+      title: "Mobile",
+      amount: 1099.0,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: "t4",
+      title: "Charger",
+      amount: 10.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: "t5",
+      title: "RAM",
+      amount: 109.9,
+      date: DateTime.now(),
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Expense Tracker"),
       ),
-      body: Center(
-        child: Text("This is body of the app"),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Card(
+              elevation: 5,
+              child: Container(
+                width: double.infinity,
+                height: 180,
+                color: Colors.blueAccent,
+                child: Text("Chart!"),
+              ),
+            ),
+            Column(
+              children: transaction.map((tx) {
+                return Card(
+                  child: Row(
+                    children: [
+                      Container(
+                        child: Text(
+                          "${tx.amount} \$",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.purple,
+                          ),
+                        ),
+                        margin:
+                            EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.purple, width: 2),
+                        ),
+                        padding: EdgeInsets.all(10),
+                      ),
+                      SizedBox(width: 10),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            tx.title,
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 17,
+                            ),
+                          ),
+                          SizedBox(height: 10),
+                          Text(
+                            "${tx.date}",
+                            style: TextStyle(
+                              fontSize: 10,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                );
+              }).toList(),
+            ),
+          ],
+        ),
       ),
     );
   }
